e893826cfa3ab3acedf89f20b04ae94c2d3631ee
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-06-14 13:53:52 +0530

    Add area of circle

---
+++ basics/area_of_circle.js
@@ -0,0 +1,12 @@
+/**
+ * Write a program to calculate the area of a circle given its radius.
+ *
+ * Area of Circle (A) = pi * r^2
+ * where,
+ *  pi = 3.14159
+ *  r = radius
+ */
+
+const area = (radius) => 3.14159 * radius ** 2;
+
+console.log(area(5));
