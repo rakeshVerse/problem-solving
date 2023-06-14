f5d5d614f45890f79a95be14b91e1405605320d2
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-06-14 14:04:06 +0530

    Add celcius to haranheit converter

---
+++ basics/temp_convert_cf.js
@@ -0,0 +1,9 @@
+/**
+ * Implement a program to convert temperature from Celsius to Fahrenheit.
+ *
+ * Formula = (0°C × 1.8) + 32 = 32°F
+ */
+
+const celToFahren = (celcius) => celcius * 1.8 + 32;
+
+console.log(celToFahren(10));
