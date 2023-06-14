4ca64176de6c5cc85affa83f4e3172480e7e2b67
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-06-14 14:08:18 +0530

    Add even odd

---
+++ basics/even_odd.js
@@ -0,0 +1,9 @@
+/**
+ * Write a program to check if a given number is even or odd.
+ *
+ */
+
+// if number conpletely divides 2 then even eles odd
+const evenOdd = (number) => (number % 2 === 0 ? "even" : "odd");
+
+console.log(evenOdd(82));
