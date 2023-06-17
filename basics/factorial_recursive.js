b35ed0ae8cfc7456d7ee7f556f4c1a7e21131a31
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-06-17 13:09:50 +0530

    Add an Example

--- basics/factorial_recursive.js
+++ basics/factorial_recursive.js
@@ -1,5 +1,20 @@
 /**
- * Factorial of 5 (5!) = 5 * 4 * 3 * 2 * 1 = 120
+ * Write a program to calculate the factorial of a given number using recursion.
+ * 
+ * Edge cases:
+ * 1. 0! = 1
+ * 2. 1! = 1
+ * 
+ * Example: 
+  Factorial of 3 (3!) = 3 * 2 * 1 = 6
+  Base case: Base case: if num is 0 or 1, factorial is 1
+  Recursive case: num * factorial(num - 1)
+
+  factorial(3) calls factorial(2)
+  factorial(2) calls factorial(1)
+  factorial(1) returns 1
+  factorial(2) = 2 * factorial(1) = 2 * 1 = 2
+  factorial(3) = 3 * factorial(2) = 3 * 2 = 6
  */
 
 const factorial = (num) => {
