c6a515729f86cf1519f76e0e2395b32c0d27e842
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-11-20 19:19:30 +0530

    Add comments on steps

--- patterns/letter-M.js
+++ patterns/letter-M.js
@@ -3,13 +3,15 @@
 /**
  * Steps:
  * - Calculate mid column (total columns must be an odd number)
+ * - For front slash, first row + last column
+ * - For back slash, row == col
  * - Draw diagonals only till row == mid column
  */
 
 const rows = 7;
 const cols = 7;
 const midCol = Math.ceil(cols / 2);
-const frontSlash = 1 + cols; // 1st row + cols (5) = 6
+const frontSlash = 1 + cols; // first row + last column
 let op = '';
 
 for (let row = 1; row <= rows; row++) {
