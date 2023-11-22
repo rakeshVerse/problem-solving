b43e228a006bda4f8bd6ab340e168b4c8d51c24f
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-11-22 16:16:23 +0530

    Faq section completed and optimize modal event handling by  creating  a single listener handling clicks on multiple elements

--- js_course_projects/netFlix/assets/js/script.js
+++ js_course_projects/netFlix/assets/js/script.js
@@ -5,25 +5,47 @@ const btnCloseModal = document.querySelector('.btn-modal-close');
 const modalLogin = document.querySelector('.modal-login');
 const backdrop = document.querySelector('.backdrop');
 
-//----------------- Modal ----------------------//
+/////////////////////// Modal ///////////////////////////
 
 const showHideModal = () => {
   modalLogin.classList.toggle('hidden');
   backdrop.classList.toggle('hidden');
 };
 
-// Open modal
-btnOpenModal.addEventListener('click', showHideModal);
-
-// Close modal
-// 'x' click
-btnCloseModal.addEventListener('click', showHideModal);
-
-// backdrop click
-backdrop.addEventListener('click', showHideModal);
+// click
+document.addEventListener('click', function (e) {
+  // when click on login btn or 'x' btn or backdrop, toggle modal
+  if (
+    e.target === btnOpenModal ||
+    e.target === btnCloseModal ||
+    e.target === backdrop
+  )
+    showHideModal();
+});
 
 // esc keypress
 document.addEventListener('keyup', e => {
   if (e.key === 'Escape' && !modalLogin.classList.contains('hidden'))
     showHideModal();
+});
+
+///////////////////////// FAQ ///////////////////////////
+
+const faqContainer = document.querySelector('.faqs-box');
+const allFaqs = document.querySelectorAll('.faq-box');
+
+faqContainer.addEventListener('click', function (e) {
+  // select closest header
+  const header = e.target.closest('header');
+  if (!header) return;
+
+  const currentFaq = header.closest('.faq-box');
+
+  // remove open class from all the faqs except the current faq
+  allFaqs.forEach(faq => {
+    if (faq !== currentFaq) faq.classList.remove('open');
+  });
+
+  // toggle class 'open' for current faq
+  currentFaq.classList.toggle('open');
 });
