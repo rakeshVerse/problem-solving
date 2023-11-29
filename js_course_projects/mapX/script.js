dae85cfee4b0cefdfc05df261ee4041948ece67e
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-11-29 18:49:56 +0530

    Get user location and load map

--- js_course_projects/mapX/script.js
+++ js_course_projects/mapX/script.js
@@ -1,15 +1,68 @@
-const typeSelectEl = document.querySelector('#workout-type');
-const runningUnitEl = document.querySelector('.cadence-box');
-const cyclingUnitEl = document.querySelector('.elevation-box');
+/////////////////////////// Toggle Candence/Elevation /////////////////
+const toggleCadenceAndElevation = () => {
+  const typeSelectEl = document.querySelector('#workout-type');
+  const runningUnitEl = document.querySelector('.cadence-box');
+  const cyclingUnitEl = document.querySelector('.elevation-box');
 
-// initially set option to running and show its input
-typeSelectEl.value = 1;
+  // initially set option to running
+  typeSelectEl.value = 1;
 
-// Show/Hide Cadence/Elevation
-const toggleUnitByType = () => {
-  runningUnitEl.classList.toggle('hidden');
-  cyclingUnitEl.classList.toggle('hidden');
+  // Show/Hide Cadence/Elevation
+  const toggleUnitByType = () => {
+    runningUnitEl.classList.toggle('hidden');
+    cyclingUnitEl.classList.toggle('hidden');
+  };
+
+  // Event: On select change, show/Hide Cadence/Elevation
+  typeSelectEl.addEventListener('change', toggleUnitByType);
+};
+/////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////// Show Map ///////////////////////////////////
+
+// LEAFLET: Load Map
+const loadMap = (lat, lng) => {
+  var map = L.map('map').setView([lat, lng], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 };
 
-// Event: On select change, show/Hide Cadence/Elevation
-typeSelectEl.addEventListener('change', toggleUnitByType);
+// GEOLOACTION API: Get user's position
+const getUserPosition = () => {
+  // On success
+  const success = position => {
+    const { latitude, longitude } = position.coords;
+
+    // load map for user's coords
+    loadMap(latitude, longitude);
+  };
+
+  // On error
+  const error = () => {
+    console.log(`Couldn't get your location!`);
+
+    // load map using london coords
+    loadMap(51.505, -0.09);
+  };
+
+  /* geolocation is available  or not*/
+  if ('geolocation' in navigator) {
+    // get user's position
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    error();
+  }
+};
+/////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////// INIT ///////////////////////////////////////////
+const init = () => {
+  toggleCadenceAndElevation();
+  getUserPosition();
+};
+
+init();
